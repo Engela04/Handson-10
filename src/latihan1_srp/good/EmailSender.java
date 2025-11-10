eac0d5e44bee705dd85018cd0c9896a845e0d088
Engela Taebenu <2482010@unai.edu>
2025-11-10 12:18:07 +0700

    Handson-10

---
+++ src/latihan1_srp/good/EmailSender.java
@@ -0,0 +1,11 @@
+
+package latihan1_srp.good;
+
+// TANGGUNG JAWAB 3: Mengirim email
+public class EmailSender {
+
+    public void send(String recipient, String message) {
+        System.out.println("[EmailSender] Sending EMAIL to: " + recipient);
+        System.out.println("[EmailSender] Content: " + message);
+    }
+}
---
+++ src/latihan1_srp/good/SmsSender.java
@@ -0,0 +1,11 @@
+
+package latihan1_srp.good;
+
+// TANGGUNG JAWAB 4: Mengirim SMS
+public class SmsSender {
+
+    public void send(String recipient, String message) {
+        System.out.println("[SmsSender] Sending SMS to: " + recipient);
+        System.out.println("[SmsSender] Content: " + message);
+    }
+}
